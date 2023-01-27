2ab205ed5bcb9728bd4e654c2a31146a2b8132fa
Ed Preston <epreston@prestonsoft.com>
2023-01-27 19:46:13 +1100

    Update test.js
    minor formatting

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 import { expect } from 'chai';
 import assert from 'assert';
 
+// ---------------------------------------------------------
+
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {

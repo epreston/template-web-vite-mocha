70dffd6c8772c021e8aa263134e693d897b64ebc
Ed Preston <epreston@prestonsoft.com>
2023-08-20 02:09:26 +1000

    Update vite.config.js
    cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 // vite.config.js
 
 import { defineConfig } from 'vite';
-// import { resolve } from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   // config options
   build: {
-    target: ['es2022', 'edge112', 'firefox112', 'chrome112', 'safari16.4', 'ios16.4'],
-  },
+    target: ['es2022', 'edge112', 'firefox112', 'chrome112', 'safari16.4', 'ios16.4']
+  }
   // server: {
   //   open: '/index.html',
   // },
